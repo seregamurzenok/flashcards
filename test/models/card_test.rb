2916c29b0d27ba1a537ffb28286b8e0c336efeb6
seregamurzenok <serega.murzenok@gmail.com>
2017-08-31 17:42:24 +0300

    Add controler test

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get :index 
+    assert_template "index"
+  end 
 end
