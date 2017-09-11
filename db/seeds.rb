91b71925ccf1a5cd48cfe25cb652e2cf71d6e239
seregamurzenok <serega.murzenok@gmail.com>
2017-09-12 00:33:47 +0300

    fourth task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' },
-#                          { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+page = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-german-words/'))
+
+lines = page.css('#post-188 > div > table > tbody > tr') 
+lines.shift #delete the table header 
+
+lines.each do |line|
+  Card.create(original_text: line.css('td')[1].text, translated_text: line.css('td')[2].text)
+end 
