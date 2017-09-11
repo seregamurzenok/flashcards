12095ccdef351959326841366fd1cb3f0d9a0113
seregamurzenok <serega.murzenok@gmail.com>
2017-09-11 18:24:15 +0300

    Fix style, blank lines

--- app/models/card.rb
+++ app/models/card.rb
@@ -8,16 +8,12 @@ class Card < ApplicationRecord
   private
 
   def set_review_date
-    create_date = Time.now
-
-    increased_day = create_date.day + 3
-    parse_str = [create_date.year, "-", create_date.month, "-", increased_day].join()
-    self.review_date = Date.parse(parse_str).strftime("%d/%m/%Y")
+    now = DateTime.current
+    self.review_date = now + 3.day
   end 
     
   def original_and_translated_texts_are_not_equal
-    if original_text.strip.capitalize == translated_text.strip.capitalize
-      errors.add(:original_text, "original and translated texts are equal")
-    end   
+    return unless original_text.strip.capitalize == translated_text.strip.capitalize
+    errors.add(:original_text, "original and translated texts are equal")   
   end 
 end
