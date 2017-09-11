038ce6629bf2f79e362741309006d8e2e5515522
seregamurzenok <serega.murzenok@gmail.com>
2017-09-12 00:36:25 +0300

    fix missing space after #

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 page = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-german-words/'))
 
 lines = page.css('#post-188 > div > table > tbody > tr') 
-lines.shift #delete the table header 
+lines.shift # delete the table header 
 
 lines.each do |line|
   Card.create(original_text: line.css('td')[1].text, translated_text: line.css('td')[2].text)
