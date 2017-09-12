5c036f53bc12296cc8f860dda9aa8931ec145feb
seregamurzenok <serega.murzenok@gmail.com>
2017-09-12 19:58:36 +0300

    create class Parser

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
+require 'parser'
 
-page = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-german-words/'))
-
-lines = page.css('#post-188 > div > table > tbody > tr') 
-lines.shift # delete the table header 
-
-lines.each do |line|
-  Card.create(original_text: line.css('td')[1].text, translated_text: line.css('td')[2].text)
-end 
+page = Parser.new('http://1000mostcommonwords.com/1000-most-common-german-words/')
+page.create_db_records 
